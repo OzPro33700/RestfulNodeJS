a321ac4d1a77f0e7ea0fe74eceefcf50b7f06256
OzPro33700 <108662137+OzPro33700@users.noreply.github.com>
2022-09-07 19:25:27 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 // run `node index.js` in the terminal
 
-// Appel du gestionnaire d’événements
-const EventEmitter = require('events');
+const http = require('http');
 
-const Logger = require('./logger');
-const logger = new Logger();
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.write('Hello World');
+    res.end();
+  }
 
-logger.on('messageLogged', (arg) => {
-  console.log('Listener called', arg);
+  if (req.url === '/api/courses') {
+    res.write(JSON.stringify([1, 2, 3]));
+    res.end();
+  }
 });
 
-logger.log('message');
+server.on('connection', (socket) => {
+  console.log('New connection');
+});
+
+server.listen(3000);
+
+console.log('Listening on port 3000');
