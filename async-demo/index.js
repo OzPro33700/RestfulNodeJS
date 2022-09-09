eab487af6374f5148ff6ee476bc3be210cafe612
OzPro33700 <108662137+OzPro33700@users.noreply.github.com>
2022-09-09 17:36:40 +0200

    2 files modified

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,35 +1,34 @@
 console.log('Before');
-getUser(1, getRepositories);
+getUser(1)
+  .then((user) => getRepositories(user.gitHubUsername))
+  .then((repos) => getCommits(repos[0]))
+  .then((commits) => console.log('Commits', commits))
+  .catch((err) => console.log('Error', err.message));
 console.log('After');
 
-function getRepositories(user) {
-  getRepositories(user.gitHubUsername, getCommits);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Reading a user from a database...');
+      resolve({ id: id, gitHubUsername: 'mosh' });
+    }, 2000);
+  });
 }
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log('Reading a user from a database...');
-    callback({ id: id, gitHubUsername: 'mosh' });
-  }, 2000);
-
-  return 1;
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Getting repositories of ${username}`);
+      resolve(['repo1', 'repo2', 'repo3']);
+    }, 2000);
+  });
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log(`Getting repositories of ${username}`);
-    callback(['repo1', 'repo2', 'repo3']);
-  }, 2000);
-
-  return 1;
-}
-
-function displayRepositories(repos, callback) {}
-
-function getCommits(repos, callback) {
-  getCommits(repo, displayCommits);
-}
-
-function displayCommits(commits) {
-  console.log(commits);
+function getCommits(repos) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      resolve(['commit']);
+    }, 2000);
+  });
 }
--- async-demo/promise.js
+++ async-demo/promise.js
@@ -1,5 +1,10 @@
 const p = new Promise((resolve, reject) => {
-  resolve(1);
+  setTimeout(() => {
+    resolve(1);
+    reject(new Error('message'));
+  }, 2000);
 });
 
-p.then((result) => console.log('Result', result));
+p.then((result) => console.log('Result', result)).catch((err) =>
+  console.log('Error', err.message)
+);
