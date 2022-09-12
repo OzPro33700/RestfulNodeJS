068816b09128c9a2d04d181e60683891762b3152
OzPro33700 <108662137+OzPro33700@users.noreply.github.com>
2022-09-12 16:34:53 +0200

    3 files modified, 5 added

---
+++ testing-demo/tests/lib.test.js
@@ -0,0 +1,5 @@
+test('Our first test', () => {
+  throw new Error('something failed');
+});
+
+test();
--- vidly/index.js
+++ vidly/index.js
@@ -1,6 +1,8 @@
+const helmet = require('helmet');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
+
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
@@ -30,12 +32,12 @@ app.use('/api/rentals', rentals);
 app.use('/api/users', users);
 
 const port = process.env.PORT || 3050;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
+
 app.use(function (req, res, next) {
   res.setHeader(
     'Content-Security-Policy',
-    "default-src 'self'; script-src 'self'; script-src-elem: 'self', style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
+    "default-src * 'unsafe-inline' 'unsafe-eval'"
   );
-
   next();
 });
-app.listen(port, () => console.log(`Listening on port ${port}...`));
