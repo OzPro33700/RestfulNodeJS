2323cf3712c163f19fb2c664ef8ecd0ad9d62eb7
OzPro33700 <108662137+OzPro33700@users.noreply.github.com>
2022-09-12 12:36:16 +0200

    3 files modified, 1 added

--- vidly/index.js
+++ vidly/index.js
@@ -5,6 +5,7 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 const express = require('express');
 const app = express();
 
@@ -15,16 +16,26 @@ const dbname = 'myFirstDatabase';
 
 mongoose
   .connect(
-    'mongodb+srv://Oz33700:rjktr754_t_A€320@cluster0.fcqcydv.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://Oz33700:rjktr754_t_A€320@cluster0.fcqcydv.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true }
   )
   .then(() => console.log('Connected to MongoDB...'))
   .catch((err) => console.error('Could not connect to MongoDB...'));
 
-/*app.use(express.json());
+app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3050;
-app.listen(port, () => console.log(`Listening on port ${port}...`));*/
+app.use(function (req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; script-src 'self'; script-src-elem: 'self', style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
+  );
+
+  next();
+});
+app.listen(port, () => console.log(`Listening on port ${port}...`));
