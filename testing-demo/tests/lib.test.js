e638d41c0336072f4f77094471994232d0e9f516
OzPro33700 <108662137+OzPro33700@users.noreply.github.com>
2022-09-14 09:05:59 +0200

    2 files modified

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -48,8 +48,17 @@ describe('getProduct', () => {
 
 describe('registerUser', () => {
   it('should throw if username is falsy', () => {
-    expect(() => {
-      lib.registerUser(null);
-    }).toThrow();
+    const args = [null, undefined, NaN, '', 0, false];
+
+    args.forEach((a) => {
+      expect(() => {
+        lib.registerUser(null);
+      }).toThrow();
+    });
+  });
+
+  it('should return a user object if valid username is passed', () => {
+    const result = lib.registerUser('mosh');
+    expect(result).toMatchObject({ username: 'mosh' });
   });
 });
