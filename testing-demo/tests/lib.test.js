e0c0dfe1b16c38c750239d35817ad9a40aa69b60
OzPro33700 <108662137+OzPro33700@users.noreply.github.com>
2022-09-14 12:22:20 +0200

    1 file modified

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,6 +1,7 @@
-const lib = require('../lib');
+//const lib = require('../lib');
+const lib = require('../exercise1');
 
-describe('absolute', () => {
+/*describe('absolute', () => {
   it('should return a positive number if input is positive', () => {
     const result = lib.absolute(1);
     expect(result).toBe(1);
@@ -62,8 +63,86 @@ describe('registerUser', () => {
     expect(result).toMatchObject({ username: 'mosh' });
     expect(result.id).toBeGreaterThan(0);
   });
-});
+});*/
 
 describe('testFizzBuzz', () => {
-  it('should raise an exception for every value whose type is not string', () => {});
+  it('should raise an exception if input is not a number', () => {
+    const args = [
+      null,
+      undefined,
+      NaN,
+      '',
+      false,
+      { a: 1 },
+      [1, 2],
+      new Error('abc'),
+    ];
+
+    args.forEach((a) => {
+      expect(() => {
+        lib.fizzBuzz(a).toThrow();
+      });
+    });
+  });
+
+  it('should return the provided input, assuming it is a number', () => {
+    const args = [Number.MIN_VALUE, -1, 0, -0.1, 0.1, 1, Number.MAX_VALUE];
+
+    args.forEach((a) => {
+      expect(() => {
+        lib.fizzBuzz(a).toBeCloseTo(a);
+      });
+    });
+  });
+
+  it('should return FizzBuzz, assuming the number is divisible both per 3 and 5', () => {
+    const args = [-15, 15];
+    const args_neg_case = [-3, -5, 3, 5];
+
+    args.forEach((a) => {
+      expect(() => {
+        lib.fizzBuzz(a).toBe('FizzBuzz');
+      });
+    });
+
+    args.forEach((a) => {
+      expect(() => {
+        lib.fizzBuzz(args_neg_case).not.toBe('FizzBuzz');
+      });
+    });
+  });
+
+  it('should return Fizz, assuming the number is divisible per 3', () => {
+    const args = [-15, -3, 3, 15];
+    const args_neg_case = [-5, 0, 5];
+
+    args.forEach((a) => {
+      expect(() => {
+        lib.fizzBuzz(a).toBe('Fizz');
+      });
+    });
+
+    args.forEach((a) => {
+      expect(() => {
+        lib.fizzBuzz(args_neg_case).not.toBe('Fizz');
+      });
+    });
+  });
+
+  it('should return Buzz, assuming the number is divisible per 5', () => {
+    const args = [-15, -5, 5, 15];
+    const args_neg_case = [-3, 0, 3];
+
+    args.forEach((a) => {
+      expect(() => {
+        lib.fizzBuzz(a).toBe('Buzz');
+      });
+    });
+
+    args.forEach((a) => {
+      expect(() => {
+        lib.fizzBuzz(args_neg_case).not.toBe('Buzz');
+      });
+    });
+  });
 });
