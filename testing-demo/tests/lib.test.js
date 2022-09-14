11322ac709e32b04d6443510866f463712c7e059
OzPro33700 <108662137+OzPro33700@users.noreply.github.com>
2022-09-14 17:06:24 +0200

    1 file modified

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,5 +1,6 @@
 const lib = require('../lib');
 const db = require('../db');
+const mail = require('../mail');
 
 /*describe('absolute', () => {
   it('should return a positive number if input is positive', () => {
@@ -158,5 +159,21 @@ describe('applyDiscount', () => {
     const order = { customerId: 1, totalPrice: 10 };
     lib.applyDiscount(order);
     expect(order.totalPrice).toBe(9);
+  });
+});
+
+describe('notifyCustomer', () => {
+  it('should send an email to the customer', () => {
+    // Enrich the DB mock, to return simply a value and make the process go through
+    db.getCustomerSync = jest.fn().mockReturnValue({ email: 'a' });
+    mail.send = jest.fn();
+
+    lib.notifyCustomer({ customerId: 1 });
+    expect(mail.send).toHaveBeenCalled();
+    /* Check arguments passed to the function:
+    First array mock.calls dimension : the call number
+    Second array mock.calls dimension : the first, second, ..., nth passed argument */
+    expect(mail.send.mock.calls[0][0]).toBe('a');
+    expect(mail.send.mock.calls[0][1]).toMatch(/order/);
   });
 });
