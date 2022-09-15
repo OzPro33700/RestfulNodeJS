65394e48da0c80f2e49db274721032bc75adf414
OzPro33700 <108662137+OzPro33700@users.noreply.github.com>
2022-09-15 17:54:29 +0200

    3 files modified, 2 added

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -1,56 +1,19 @@
-const mongoose = require('mongoose');
-
-const username = 'Oz33700';
-const password = 'rjktr754_t_A€320';
-const cluster = 'cluster0.fcqcydv';
-const dbname = 'myFirstDatabase';
-
-mongoose
-  .connect(
-    `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => console.log('Connected to MongoDB...'))
-  .catch((err) => console.log('Could not connect to MongoDB...', err));
-
-const User = mongoose.model(
-  'User',
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-    email: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 255,
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 1024,
-    },
-  })
-);
-
-function validateUser(genre) {
-  const schema = {
-    name: Joi.string().min(5).max(50).required(),
-    email: Joi.string().min(5).max(255).required().email(),
-    password: Joi.string().min(5).max(255).required(),
-  };
-
-  return Joi.validate(genre, schema);
+const { MongoClient } = require('mongodb');
+// Replace the uri string with your connection string.
+const uri =
+  'mongodb+srv://Oz33700:Im0oydCxLMN5J3Ch@cluster0.fcqcydv.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
-
-exports.User = User;
-exports.validate = validateUser;
+run().catch(console.dir);
